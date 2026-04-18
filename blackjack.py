a39607256f7b68a3470c69454047106309a337e2
foojoel <foojoel@gmail.com>
2026-04-17 18:41:01 -0700

    Added interactivity via the blackjackapp command, refactored some things, should be playable

--- blackjack.py
+++ blackjack.py
@@ -4,31 +4,14 @@ import cards_api as ca
 
 game = "blackjack"
 
-session = ca.new_table(game)
-
-session_id = session["deck_id"]
-print(session_id)
-
-player_hand = ca.draw_cards(session_id, game)
-
-dealer_hand = ca.draw_cards(session_id, game)
-
-print(player_hand)
-
-print(dealer_hand)
-
 def hand_eval(hand):
     hand_value = [x[0] for x in hand]
     hand_score = 0
     ace_count = 0
     for c in hand_value:
-        if c in ['J', 'Q', 'K']:
-            hand_score += 10
-        elif c == 'A':
-            hand_score += 11            
+        hand_score += ca.card_values[c]
+        if c == 'A':           
             ace_count += 1
-        else:
-            hand_score += int(c)
 
     while hand_score > 21 and ace_count > 0:
         hand_score -= 10
@@ -46,4 +29,96 @@ def score_round(player, dealer):
         victory = True
     return victory
 
-print(score_round(player_hand, dealer_hand))
+#print(score_round(player_hand, dealer_hand))
+
+def startgame_call(blackjackobj):
+    blackjackobj.startgame()
+    print("The dealer's hand is:")
+    print(ca.hand_display(blackjackobj.dealer_hand))
+    print("The player's hand is: ")
+    print(ca.hand_display(blackjackobj.player_hand))
+
+def hitme_call(blackjackobj):
+    blackjackobj.hitme()
+    print("The player's hand is:")
+    print(ca.hand_display(blackjackobj.player_hand))
+
+def stand_call(blackjackobj):
+    blackjackobj.stand()
+    print("The game result is:")
+    if blackjackobj.stand():
+        print("Player win")
+    else:
+        print("Dealer win")
+
+def exitgame_call():
+    print("Okay")
+
+def instructions_call():
+        print("Here are your options. Input command in text:")
+        print("1. Start")
+        print("2. Hit")
+        print("3. Stand")
+        print("4. Instructions")
+        print("5. Exit")
+# Wrapper functions
+commands = {
+    "start": startgame_call,
+    "hit": hitme_call,
+    "stand": stand_call,
+    "instructions": instructions_call,
+    "exit": exitgame_call,    
+}
+
+class BlackjackGame:
+    def __init__(self):
+        self.deck_id = ca.new_table(game)["deck_id"]
+        self.player_hand = []
+        self.dealer_hand = []
+        self.game_count = 0
+        self.active_round = False
+
+    def startgame(self):
+        if self.active_round == True:
+            print("There is already a game in progress!")
+        else:
+            ca.shuffle(self.deck_id)
+            self.player_hand = ca.draw_cards(self.deck_id, game)
+            self.dealer_hand = ca.draw_cards(self.deck_id, game)
+            self.active_round = True
+
+    def hitme(self):
+        if hand_eval(self.player_hand) <= 21:
+            self.player_hand = ca.hit_me(self.deck_id, self.player_hand)
+        else:
+            print("Player bust")
+    
+    def stand(self):
+        while hand_eval(self.dealer_hand) < 17:
+            self.dealer_hand = ca.hit_me(self.deck_id, self.dealer_hand)
+    
+    def endround(self):
+        self.game_count += 1
+        self.active_roud = False
+        return score_round(self.player_hand, self.dealer_hand)
+
+
+def blackjack_app():
+    currentgame = BlackjackGame()
+    print("Welcome to the Blackjack Table")
+    instructions_call()
+
+    while True:
+        choice = input("What would you like to do? ").strip().lower()
+        action = commands.get(choice)
+        if choice == "exit":
+            break
+        elif choice in commands: # need to fix using wrapper functions
+            action(currentgame)
+        else:
+            print("Unexpected command, break initiated")
+            break
+
+        
+
+blackjack_app()
--- cards_api.py
+++ cards_api.py
@@ -1,26 +1,54 @@
 import requests
 
-card_suits = {
+card_suits_display = {
     "S": "Spades",
     "H": "Hearts",
     "C": "Clubs",
     "D": "Diamonds",
 }
 
-#Aces high for now
-card_faces = {
+# Aces high for now
+card_values = {
+    "1": 1,
+    "2": 2,
+    "3": 3,
+    "4": 4,
+    "5": 5,
+    "6": 6,
+    "7": 7,
+    "8": 8,
+    "9": 9,
+    "0": 10,
     "J": 10,
     "Q": 10,
     "K": 10,
     "A": 11,
 }
 
+card_values_display = {
+    "1": "One",
+    "2": "Two",
+    "3": "Three",
+    "4": "Four",
+    "5": "Five",
+    "6": "Six",
+    "7": "Seven",
+    "8": "Eight",
+    "9": "Nine",
+    "0": "Ten",
+    "J": "Jack",
+    "Q": "Queen",
+    "K": "King",
+    "A": "Ace",
+}
+
+
 baseurl = "https://deckofcardsapi.com/api/deck/"
 
 #blackjack uses multiple (6?)
 deck_count = {
     "poker": 1,
-    "blackjack": 6,
+    "blackjack": 1, # Should be more, but right now not necessary
 }
 
 game_draw = {
@@ -44,11 +72,30 @@ def hit_me(deck_id, current_hand):
     temp_hand = [x["code"] for x in hand_data["cards"]]
     return current_hand + temp_hand
 
-#does not work for multiple decks (i.e. blackjack)
+# Does not work for multiple decks (i.e. blackjack)
 def discard(deck_id, gametype, pile, hand):
     hand_input = ",".join(hand)
     requests.get(baseurl + deck_id + "/pile/" + pile + "/add/?cards=" + hand_input)
 
+#
 def deck_status(deck_id):
     status = requests.get(baseurl + deck_id + "/shuffle/?remaining=true")
     return status.json()["remaining"]
+
+# Returns all cards to deck to shuffle
+def shuffle(deck_id):
+    status1 = requests.get(baseurl + deck_id + "/return/")
+    status2 = requests.get(baseurl + deck_id + "/shuffle/")
+
+# A card is a two character string
+def card_display(card):
+    return card_values_display[card[0]] + " of " + card_suits_display[card[1]]
+
+# A hand is a list of cards
+def hand_display(hand):
+    hand_value = ""
+    for c in hand:
+        hand_value += card_display(c) + "; "
+    return hand_value
+        
+
